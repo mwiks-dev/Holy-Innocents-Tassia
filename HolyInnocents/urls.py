071d8bba1ee46274da1384b29895b622c5a74d9c
Maryann Mwikali <mwikali119@gmail.com>
2023-05-23 12:16:13 +0300

    Configure timezone,url and run first migration

--- HolyInnocents/urls.py
+++ HolyInnocents/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
+    path("", include("HolyInnocentsApp.urls")),
     path('admin/', admin.site.urls),
 ] + staticfiles_urlpatterns()
